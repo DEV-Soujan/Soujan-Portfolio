43c1440a1fe73540e5194dbe396bdb596c66cb96
DEV-Soujan <80923048+DEV-Soujan@users.noreply.github.com>
2023-05-22 16:20:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+/* $('.showcase').mouseover(function () {
+    $('.showcase').css({
+        'filter': 'blur(10px)',
+        '-webkit-filter': 'blur(10px)'
+    });
+});
+
+$('body').mouseover(function () {
+    $('.showcase').css({
+       'filter': 'blur(0px)',
+       '-webkit-filter': 'blur(0px)'
+    });
+}); 
+
+*/ 
+
+
